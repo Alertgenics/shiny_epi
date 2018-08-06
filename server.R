892a3be88e4e23559314af5a246c5abb2b4581b2
Alertgenic <addiana.ruiz@gmail.com>
2018-08-06 18:08:52 +0200

    server changes

--- server.R
+++ server.R
@@ -5,19 +5,43 @@
 # http://shiny.rstudio.com
 #
 
+
 library(shiny)
 
-shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+function(input, output) {
+  
+  ## Interactive Map ###########################################
+  
+  # Create the map
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
+      setView(lng = -93.85, lat = 37.45, zoom = 3)
+    
   })
-
-})
+  
+  #Read datainput
+  read_excel_allsheets <- function(filename) {
+    sheets <- readxl::excel_sheets(filename)
+    x <-    lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+    names(x) <- sheets
+    x
+  }
+  read_excel_selectedSheet <- function(filename) {
+    age <- input$age
+    x <-    lapply(sheets, function(X) readxl::read_excel(filename, sheet = age))
+  }
+  #show table button interaction
+  observeEvent(input$show, {
+    showModal(modalDialog(
+      title = "Influenza Data"
+      
+    ))
+  })
+  #Datatable
+  
+  
+}
