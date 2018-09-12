f099cb2a1c5c338596b776b22a246a11443ec67c
Alertgenic <addiana.ruiz@gmail.com>
2018-09-12 19:22:43 +0200

    New functions

--- server.R
+++ server.R
@@ -11,12 +11,70 @@ library(readxl)
 library(leaflet)
 library(ggmap)
 library(dplyr)
+library(DT)
+library(ggplot2)
+library(tidyr)
+
 
 function(input, output) {
   
-  ## Interactive Map ###########################################
-
   
+  # Create one data frame for each sheet
+  #For Murders
+  tidy_excel <- function(filename) {
+    sheets_names <- excel_sheets(filename)
+    num_sheets <- length(sheets_names)
+    ## preparamos un dataframe vacio para poder hacer el bind_rows incremental
+    prueba1 <- tibble(
+      Year = numeric(0),
+      Month = numeric(0),
+      Country = character(0),
+      measurement = numeric(0),
+      sheet_name = character(0)
+    )
+    
+    for(i in 1:num_sheets){
+      df <- read_excel(filename, sheet = i)
+      df_long <- gather(
+        df,
+        key = "Country",
+        value = "measurement",
+        - Year,
+        - Month)
+      df_long$sheet_name <- sheets_names[i]
+      prueba1 <- bind_rows(prueba1, df_long)
+    }
+    prueba1
+    
+  }
+  # For some healthcare
+  tidy_excel_healthcare <- function(filename) {
+    sheets_names <- excel_sheets(filename)
+    num_sheets <- length(sheets_names)
+    ## preparamos un dataframe vacio para poder hacer el bind_rows incremental
+    prueba <- tibble(
+      Year = numeric(0),
+      Month = numeric(0),
+      Region = character(0),
+      measurement = numeric(0),
+      sheet_name = character(0)
+    )
+    
+    for(i in 1:num_sheets){
+      df <- read_excel(filename, sheet = i)
+      df_long <- gather(
+        df,
+        key = "Region",
+        value = "measurement",
+        - Year,
+        - Month)
+      df_long$sheet_name <- sheets_names[i]
+      prueba1 <- bind_rows(prueba1, df_long)
+    }
+    prueba1
+    
+  }
+  ## Interactive Map ###########################################
   # Create the map
   output$map <- renderLeaflet({
     pais <- input$country
@@ -50,7 +108,7 @@ function(input, output) {
     }
     
   })
-  
+
   #Read datainput
   read_excel_allsheets <- function(filename) {
     sheets <- readxl::excel_sheets(filename)
@@ -63,12 +121,32 @@ function(input, output) {
     x <-   readxl::read_excel(filename, sheet = sheets)
     
   }
+  read_excel_selectedSheet2 <- function(filename) {
+    sheets <- input$sheet
+    x <-   readxl::read_excel(filename, sheet = sheets)
+    
+  }
+  read_excel_someSelectedSheet2 <- function(filename, tibble = FALSE) {
+    # I prefer straight data.frames
+    # but if you like tidyverse tibbles (the default with read_excel)
+    # then just pass tibble = TRUE
+    sheets <- readxl::excel_sheets(filename)
+    x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+    if(!tibble) x <- lapply(x, as.data.frame)
+    names(x) <- sheets
+    x
+  }
   #show table button interaction
   observeEvent(input$show, {
     showModal(modalDialog(
       title = "Influenza Data",
       dataTableOutput("mytable")
     ))
+  })
+  observeEvent(input$show2,{
+    showModal(modalDialog(title= "Comparative Table",
+              dataTableOutput("compTable")
+              ))
   })
   #Datatable
   output$mytable <- renderDataTable({
@@ -111,7 +189,21 @@ function(input, output) {
       
    
   })
-
+  output$compTable<- renderDataTable({
+    inFile <- input$file
+    age <- input$age3
+    if(is.null(inFile)){
+      return(NULL)
+    }
+    file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
+    path1 <- read_excel_someSelectedSheet2(paste(inFile$datapath, ".xls", sep=""))
+   # table <- map2_df(path1,)
+    #if(!is.null(age)){
+      #for(edad in age[0]:length(age)){
+       # table <- 
+      #}
+    #}
+})
   output$selector <- renderUI({
     countries <- read_excel("data/countries.xls",col_names = TRUE)
     if(is.null(input$country)||input$country == ""){
@@ -125,11 +217,76 @@ function(input, output) {
       inputId = "region1", 
       label = "Region:",
       choices = region$States,
-      selected = region$States[1],
+      selected = NULL,
       multiple = TRUE)
     }
   })
-  #Histogram 1
+  output$ages <- renderUI({
+    inFile <- input$file
+    if(is.null(inFile)){
+      selectInput("age3","Ages", c("All ages"="", "less 1 year" = "less 1 year","01-04" = "01-04","05-09" = "05-09","10-14" = "10-14","15-19" = "15-19","20-29" = "20-29",
+                                 "30-39" = "30-39","40-49" = "40-49","50-59" = "50-59","60-69" = "60-69","70-79" = "70-79","80 and plus" = "80 and plus"), multiple=FALSE)
+      
+     }else{
+    file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
+    sheet <- excel_sheets(paste(inFile$datapath, ".xls", sep=""))
+    selectInput(
+      inputId = "age3", 
+      label = "Ages:",
+      choices = sheet,
+      selected = NULL,
+      multiple = TRUE)
+    }
+  })
+  output$sheet <- renderUI({
+    inFile <- input$file
+    if(is.null(inFile)){
+      #"First select a compatible file. Please."
+      selectInput("sheet","Virus Type:", c("All Virus"=""))
+    }else{
+    file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
+    sheets <- excel_sheets(paste(inFile$datapath, ".xls", sep=""))
+    selectInput(
+      inputId = "sheet", 
+      label = "Virus Type:",
+      choices = sheets,
+      selected = NULL,
+      multiple = TRUE)
+    }
+  })
+  output$yearsM <- renderUI({
+    inFile <- input$file
+    
+    if(is.null(inFile)){
+      
+      selectInput("years","Years:", c("All years"="","1998"="1998","1999"="1999", "2000"="2000","2001"="2001","2002"="2002","2003"="2003","2004"="2004","2005"="2005",
+                                     "2006"="2006","2007"="2007","2008"="2008","2009"="2009","2010"="2010","2011"="2011","2012"="2012","2013"="2013"), multiple=TRUE)
+    }else{
+      if(length(input$sheet) >1){
+        file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
+        excel <- read_excel_someSelectedSheet2(paste(inFile$datapath, ".xls", sep=""))
+        selectInput(
+          inputId = "years", 
+          label = "Years:",
+          choices = excel$Year,
+          selected = NULL,
+          multiple = TRUE)
+      }else{
+        
+      
+      file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
+      excel <- read_excel_selectedSheet2(paste(inFile$datapath, ".xls", sep=""))
+      selectInput(
+        inputId = "years", 
+        label = "Years:",
+        choices = excel$Year,
+        selected = NULL,
+        multiple = TRUE)
+      }
+    }
+  })
+
+  #Plot (with 1 sheet) for filter1
   output$plot<- renderPlot({
     inFile <- input$file
     region1 <- input$region1
@@ -137,7 +294,8 @@ function(input, output) {
       file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
       table <- read_excel_selectedSheet(paste(inFile$datapath, ".xls", sep=""))
       x<- table %>% select(region1)
-      x.ts <- ts(x,start = c(1998,1), frequency = 12 )
+      anio <- table[1, "YEAR"]
+      x.ts <- ts(x,start = c(anio$YEAR,1), frequency = 12 )
         plot(x.ts, plot.type = "single", col=sample(rainbow(20)))
         legend(x = 1,y =  100, legend=region1,col=1:20)
         #descomp2=stl(x.ts,s.window="periodic")
@@ -146,19 +304,45 @@ function(input, output) {
         file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
         table <- read_excel_selectedSheet(paste(inFile$datapath, ".xls", sep=""))
         x<- table
-        x.ts <- ts(x,start = c(1998,1), frequency = 12 )
+        anio <- table[1, "YEAR"]
+        x.ts <- ts(x,start = c(anio$YEAR,1), frequency = 12 )
         plot(x.ts, plot.type = "single", col=sample(rainbow(20)))
       }
 
   })
- output$autocorrelacion <- renderPlot({
+  # Plot for Murderer Filter
+  output$plot3<- renderPlot({
+   inFile <- input$file
+   virus <- input$sheet
+   countr <- input$country2
+   
+     # Sheets == 1
+         file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
+         prueba1 <- tidy_excel(paste(inFile$datapath, ".xls", sep=""))
+         if(!is.null(virus)){
+          if(!is.null(countr)){
+           
+           x <- filter(prueba1, Country == countr)
+           y <- filter(x, sheet_name == virus)
+           
+           plot(y$Year,y$measurement) #nube de puntos
+           
+          }else{
+            x <- filter(prueba1, sheet_name == virus)
+         }
+         
+       }
+
+ })
+  output$autocorrelacion <- renderPlot({
    inFile <- input$file
    region1 <- input$region1
    if(!is.null(region1)){
      file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
      table <- read_excel_selectedSheet(paste(inFile$datapath, ".xls", sep=""))
      x<- table %>% select(region1)
-     x.ts <- ts(x,start = c(1998,1), frequency = 12 )
+     anio <- table[1, "YEAR"]
+     x.ts <- ts(x,start = c(anio$YEAR,1), frequency = 12 )
      acf(x.ts) #autocorrelacion
      #lag.plot(x.ts, main="dfsd") #nube de puntos
      #Descomposición de una serie temporal
@@ -167,14 +351,15 @@ function(input, output) {
      
    }
  })
- output$decomposition <- renderPlot({
+  output$decomposition <- renderPlot({
    inFile <- input$file
    region1 <- input$region1
    if(!is.null(region1)){
      file.rename(inFile$datapath,paste(inFile$datapath, ".xls", sep=""))
      table <- read_excel_selectedSheet(paste(inFile$datapath, ".xls", sep=""))
      x<- table %>% select(region1)
-     x.ts <- ts(x,start = c(1998,1), frequency = 12 )
+     anio <- table[1, "YEAR"]
+     x.ts <- ts(x,start = c(anio$YEAR,1), frequency = 12 )
      #acf(x.ts) #autocorrelacion
      #lag.plot(x.ts, main="dfsd") #nube de puntos
      #Descomposición de una serie temporal
--- ui.R
+++ ui.R
@@ -43,20 +43,25 @@ navbarPage("Epipoi", id="nav",
                         leafletOutput("map", width="100%", height="100%"),
                         absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
                                       draggable = TRUE, top = 60, left = "auto", right = 20, bottom = 20,
-                                      width = 330, height = "auto",
+                                      width = 380, height = "auto",
                                       
                                       h2("Epidemial explorer"),
                                       fileInput("file", "Select a file:",
                                                 multiple = TRUE,
                                                 accept = c(".xls",".XLS", ".xlsx")),
-                                      
+                                      fluidRow(
+                                      column(3,
                                       checkboxInput(inputId = "filters",
                                                     label = strong("Filters"),
-                                                    value = FALSE),
-                                      checkboxInput(inputId = "compareFilters",
-                                                    label = strong("Compare 2 range of ages"),
-                                                    value = FALSE),
-                                      
+                                                    value = FALSE)),
+                                      column(3,checkboxInput(inputId = "murder",
+                                                             label = strong("Mundial murders"),
+                                                             value = FALSE)),
+                                      column(3,checkboxInput(inputId = "compareFilters",
+                                                    label = strong("Filters 2:"),
+                                                    value = FALSE))
+                                      ),
+                                    
                                       conditionalPanel(condition = "input.filters == true",
                                                        # Download country mappings
                                                        h4("Filters:"),
@@ -80,7 +85,13 @@ navbarPage("Epipoi", id="nav",
                                                        checkboxInput(inputId = "decomposition",
                                                                      label = strong("Decomposition of additive time series"),
                                                                      value = FALSE),
-                                                       conditionalPanel(condition = "input.compareHist1 == true && input.disperson1 == false && input.decomposition == false",
+                                                       checkboxInput(inputId = "wave",
+                                                                     label = strong("Wavelet"),
+                                                                     value = FALSE),
+                                                       checkboxInput(inputId = "grid",
+                                                                     label = strong("Grid"),
+                                                                     value = FALSE),
+                                                       conditionalPanel(condition = "input.compareHist1 == true && input.disperson1 == false && input.decomposition == false && input.wave==false && input.grid == false",
                                                                         # Download country mappings
                                                                         absolutePanel(id = "Histogram1", class = "panel panel-default", fixed = TRUE,
                                                                                       draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
@@ -88,22 +99,38 @@ navbarPage("Epipoi", id="nav",
                                                                                       plotOutput("plot")
                                                                                       
                                                                         )),
+                                                       conditionalPanel(condition = "input.compareHist1 == false && input.disperson1 == false && input.decomposition == false && input.wave==true && input.grid == false",
+                                                                        # Download country mappings
+                                                                        absolutePanel(id = "Histogram1", class = "panel panel-default", fixed = TRUE,
+                                                                                      draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
+                                                                                      width = 600, height = "auto",
+                                                                                      plotOutput("plot1")
+                                                                                      
+                                                                        )),
+                                                       conditionalPanel(condition = "input.compareHist1 == false && input.disperson1 == false && input.decomposition == false && input.wave==false && input.grid == true",
+                                                                        # Download country mappings
+                                                                        absolutePanel(id = "Histogram1", class = "panel panel-default", fixed = TRUE,
+                                                                                      draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
+                                                                                      width = 600, height = "auto",
+                                                                                      plotOutput("plot2")
+                                                                                      
+                                                                        )),
                                                       
-                                                       conditionalPanel(condition = "input.disperson1 == true && input.compareHist1 == false && input.decomposition == false",
+                                                       conditionalPanel(condition = "input.disperson1 == true && input.compareHist1 == false && input.decomposition == false && input.wave==false && input.grid == false",
                                                                         # Download country mappings
                                                                         absolutePanel(id = "Dispersion", class = "panel panel-default", fixed = TRUE,
                                                                                       draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
                                                                                       width = 600, height = "auto",
                                                                                       plotOutput("autocorrelacion")
                                                                         )),
-                                                       conditionalPanel(condition = "input.disperson1 == false && input.compareHist1 == false && input.decomposition == true",
+                                                       conditionalPanel(condition = "input.disperson1 == false && input.compareHist1 == false && input.decomposition == true && input.wave==false && input.grid == false",
                                                                         # Download country mappings
                                                                         absolutePanel(id = "Dispersion", class = "panel panel-default", fixed = TRUE,
                                                                                       draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
                                                                                       width = 600, height = "auto",
                                                                                       plotOutput("decomposition")
                                                                         )),
-                                                       conditionalPanel(condition = "input.disperson1 == true && input.compareHist1 == true && input.decomposition == true",
+                                                       conditionalPanel(condition = "input.disperson1 == true && input.compareHist1 == true && input.decomposition == true && input.wave==false && input.grid == false",
                                                                         # Download country mappings
                                                                         absolutePanel(id = "Dispersion", class = "panel panel-default", fixed = TRUE,
                                                                                       draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
@@ -115,10 +142,11 @@ navbarPage("Epipoi", id="nav",
                                                     conditionalPanel(condition = "input.compareFilters == true",
                                                        # Download country mappings
                                                        h4("Filters:"),
-                                                       selectInput("age1","Age", c("All ages"="", "less 1 year" = "less 1 year","01-04" = "01-04","05-09" = "05-09","10-14" = "10-14","15-19" = "15-19","20-29" = "20-29",
-                                                                                  "30-39" = "30-39","40-49" = "40-49","50-59" = "50-59","60-69" = "60-69","70-79" = "70-79","80 and plus" = "80 and plus"), multiple=FALSE),
-                                                       selectInput("age2","Age", c("All ages"="", "less 1 year" = "less 1 year","01-04" = "01-04","05-09" = "05-09","10-14" = "10-14","15-19" = "15-19","20-29" = "20-29",
-                                                                                  "30-39" = "30-39","40-49" = "40-49","50-59" = "50-59","60-69" = "60-69","70-79" = "70-79","80 and plus" = "80 and plus"), multiple=FALSE),
+                                                       htmlOutput("ages"),
+                                                       #selectInput("age1","Age", c("All ages"="", "less 1 year" = "less 1 year","01-04" = "01-04","05-09" = "05-09","10-14" = "10-14","15-19" = "15-19","20-29" = "20-29",
+                                                       #                           "30-39" = "30-39","40-49" = "40-49","50-59" = "50-59","60-69" = "60-69","70-79" = "70-79","80 and plus" = "80 and plus"), multiple=FALSE),
+                                                       #selectInput("age2","Age", c("All ages"="", "less 1 year" = "less 1 year","01-04" = "01-04","05-09" = "05-09","10-14" = "10-14","15-19" = "15-19","20-29" = "20-29",
+                                                       #                           "30-39" = "30-39","40-49" = "40-49","50-59" = "50-59","60-69" = "60-69","70-79" = "70-79","80 and plus" = "80 and plus"), multiple=FALSE),
                                                        
                                                        selectInput("month2","Month", c("All months"="", "January" = "1","February" = "2","March" = "3","April" = "4","May" = "5","June" = "6",
                                                                                       "July" = "7","August" = "8","September" = "9","October" = "10","November" = "11","December" = "12"), multiple=TRUE),
@@ -131,9 +159,37 @@ navbarPage("Epipoi", id="nav",
                                                       # htmlOutput("selector2"),
                                                       
                                                        actionButton("show2", "Show Comparative Data Table")
+                                                       ),
+                                      conditionalPanel(condition = "input.murder == true", 
+                                                       h4("Filters:"),
+                                                       htmlOutput("sheet"),
+                                                       htmlOutput("yearsM"),
+                                                       selectInput("country2","Country", c("All countries"="",structure(countries$Country, names=countries$Country)), multiple=FALSE),
+                                                       
+                                                       checkboxInput(inputId = "Hist1",
+                                                                     label = strong("temporal"),
+                                                                     value = FALSE),
+                                                       checkboxInput(inputId = "disperso",
+                                                                     label = strong("Disperson map"),
+                                                                     value = FALSE),
+
+                                                       conditionalPanel(condition = "input.Hist1 == true && input.disperso == false",
+                                                                        # Download country mappings
+                                                                        absolutePanel(id = "Histogram1", class = "panel panel-default", fixed = TRUE,
+                                                                                      draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
+                                                                                      width = 600, height = "auto",
+                                                                                      plotOutput("plot3")
+                                                                                      
+                                                                        )),
+                                                       
+                                                       conditionalPanel(condition = "input.disperso == true && input.Hist1 == false",
+                                                                        # Download country mappings
+                                                                        absolutePanel(id = "Dispersion", class = "panel panel-default", fixed = TRUE,
+                                                                                      draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
+                                                                                      width = 600, height = "auto",
+                                                                                      plotOutput("autocorrelacion1")
+                                                                        ))
                                                        )
-                                      
-                                      
                         )
                     )
            )
